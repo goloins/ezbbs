76b3fb8da0dbeddef332c1a4736bf125d270a284
goloins <dsides.personal@gmail.com>
2026-03-18 14:58:17 -0500

    init.php: initial settings for site/nonlogged users

---
+++ init.php
@@ -0,0 +1,20 @@
+<?php
+/* ezbbs - a simple bbs engine for the small web 
+ *
+ * Copyleft 2026 by the ezbbs contributors
+ * 
+ * index.php - main page of the site, showing all threads from all cats
+ * 
+ */
+
+
+$site = array(
+    'site_name' => 'EzBBS',
+    'site_description' => 'A simple bbs engine for the small web',
+    'favicon_url' => 'assets/favicon.ico',
+    'site_url' => 'https://localhost/'
+    );
+
+$user = array(
+    'theme' => 'default'
+);
