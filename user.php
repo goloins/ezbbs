7e12d88aa7899f4612615a850061042a46f48f3c
goloins <dsides.personal@gmail.com>
2026-03-19 09:48:04 -0500

    user.php: added styling to username in title area.

--- user.php
+++ user.php
@@ -14,12 +14,24 @@ if(!isset($_GET['id'])) {
 
 
 require_once 'init.php';
+//now we get all the user shit
+
+$userId = intval($_GET['id']);
+$profileUser = do_getUserById($userId);
+if(!$profileUser) {
+    header('Location: /404/user'); //specific 404 page for users, so we can say "user not found" instead of just "page not found"
+    exit();
+}
+
+//so we should now have all of the user info.
+
 ?>
+
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id="top">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
-		<title>Latest threads — <?php echo $site['site_name'];?></title>
+		<title>Latest threads — <?php echo 'Profile for ' . $profileUser['username'] . ' — ' . $site['site_name']; ?></title>
                     <meta name="description" content="<?php echo $site['site_description'];?>"/>
                 <link rel="icon" type="image/gif" href="<?php echo $site['favicon_url'];?>" />
 		<link rel="stylesheet" type="text/css" media="screen" href="assets/css/layout.css" />
@@ -37,16 +49,29 @@ foreach($homepagemenu as $menu_item) {
 ?>
 </ul>
 </div>
-<?php
-//now we get all the user shit
-
-
-?>
 
 
 <div id="body_wrapper">
     	<h2 id="body_title">
+        <?php 
+        $fullusername = "";
+        if(isset($profileUser['usernamecolor'])) {
+            $fullusername .= '<span style="color:' . $profileUser['usernamecolor'] . '">';
+        }
+        if(isset($profileUser['usernamestyle'])) {
+            $fullusername .= '<span style="' . $profileUser['usernamestyle'] . '">';
+        }
+        $fullusername .= $profileUser['username'];
+        if(isset($profileUser['usernamestyle'])) {
+            $fullusername .= '</span>';
+        }
+        if(isset($profileUser['usernamecolor'])) {
+            $fullusername .= '</span>';
+        }
+        echo 'Profile for ' . $fullusername;
+        //now to figure out where to put their awards
         
+        ?>
 
 
 </h2>
