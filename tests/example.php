20028d5eb22f2dda8a40eccb7818b3796d17878a
goloins <dsides.personal@gmail.com>
2026-03-19 15:50:25 -0500

    tests: added example test file with warning and ideas

---
+++ tests/example.php
@@ -0,0 +1,36 @@
+<?php
+/* ezbbs - a simple bbs engine for the small web 
+ *
+ * Copyleft 2026 by the ezbbs contributors
+ * 
+ * This is part of the ezbbs test suite. during install, you must delete this folder.
+ * The functions referrenced in this file are not protected by any sort of permissions
+ * because of the immaturity and alpha status of the software. that being said,
+ * 
+ *____  _____ _     _____ _____ _____   _____ ___  _     ____  _____ ____  
+ *|  _ \| ____| |   | ____|_   _| ____| |  ___/ _ \| |   |  _ \| ____|  _ \ 
+ *| | | |  _| | |   |  _|   | | |  _|   | |_ | | | | |   | | | |  _| | |_) |
+ *| |_| | |___| |___| |___  | | | |___  |  _|| |_| | |___| |_| | |___|  _ < 
+ *|____/|_____|_____|_____|_|_| |_____| |_| _ \___/|_____|____/|_____|_| \_\
+ *| __ )| ____|  ___/ _ \|  _ \| ____| | | | / ___|| ____| |                
+ *|  _ \|  _| | |_ | | | | |_) |  _|   | | | \___ \|  _| | |                
+ *| |_) | |___|  _|| |_| |  _ <| |___  | |_| |___) | |___|_|                
+ *|____/|_____|_|   \___/|_| \_\_____|  \___/|____/|_____(_)               
+ * 
+ * 
+ * /tests/example.php - example test file for ezbbs test suite.
+ * 
+ * This file is meant to be a simple example of how to write a test for ezbbs. 
+ * The functions used here are not protected by any sort of permissions, so you can do whatever you want in this file.
+ * That being said, try to keep it somewhat sane and remember to delete this folder once you're done fucking around.
+ * 
+ * 
+ * In similar spirit to the /do/ framework, these files will pretty much just pull in init.php and do simple, 
+ * specific tasks. You can automate posting, user creation, spam prevention, whatever. The sky's your oyster.
+ */
+
+
+include('../init.php');
+
+
+echo 'hello :^)';
